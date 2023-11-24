04f42a83b973c9a4ec5aa946e68490c9cda380d8
Rohini Kawadkar <kawadkarrenu@gmail.com>
2023-11-24 18:58:19 +0530

    add

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Home } from './Home';
 import MultipleState from './MultipleState';
+import { APIDisplay } from './APIDisplay';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MultipleState />
+    <APIDisplay />
   </React.StrictMode>
 );
 
