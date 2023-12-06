7d78b5e8330670885a6ef8b76029886c0b417a50
Rohini Kawadkar <kawadkarrenu@gmail.com>
2023-12-06 18:23:50 +0530

    add

--- src/axios/registration.js
+++ src/axios/registration.js
@@ -9,8 +9,12 @@ export default function RegistrationForm() {
 
     
    const [data,setData] = useState({
+    firstName:"",
+    lastName:"",
     email:"",
     password:"",
+    Confirmpassword:"",
+    acceptTerm:""
 
     })
 
@@ -40,9 +44,13 @@ export default function RegistrationForm() {
     <>
 
 <form onSubmit={handleSubmit}>
-
+<input type='text' name="firstName" value={data.firstName}  onChange={handleInput}/>
+<input type='text' name="lastName" value={data.lastName}  onChange={handleInput}/>
 <input type='text' name="email" value={data.email}  onChange={handleInput}/>
 <input type='text' name='password'  value={data.password}  onChange={handleInput}/>
+<input type='text' name="Confirmpassword" value={data.Confirmpassword}  onChange={handleInput}/>
+
+<input type='checkbox' name="acceptTerm" value={data.acceptTerm}  onChange={handleInput}/>
 <button type='submit' value="save"><Link to="/logIn">SignIn</Link></button>
 </form>
 
